88a84845c2877676d7c63d97c580bf028870726e
Fernando Aguilar <feraguilar6985@gmail.com>
2023-04-17 22:53:47 +0000

    add basic variables types

---
+++ src/variables.rs
@@ -0,0 +1,21 @@
+fn variables() {
+     // Variables
+
+    // Unsigned numbers u8, U16, U32, U64, U128
+    // Signed numbers i8, i16, i32, i64, i128
+    let age: i64 = 23+2;
+
+    // Floats f32, f64
+    let weight: f32 = 65.5;
+
+    // String & str
+    let name: &str = "Fernando Aguilar";
+
+    // Boolean
+    let is_active: bool = true;
+
+    // Print
+    println!("Hi there! I'm {} My Age is: {} years old", name, age);
+    println!("My weight is: {} kg", weight);
+    println!("Is active? {}", is_active);
+}
