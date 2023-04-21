820d4b16e8cd38c180a11971eab3a9e9bf24d162
Fernando Aguilar <feraguilar6985@gmail.com>
2023-04-21 21:26:01 +0000

    add functions

---
+++ src/functions.rs
@@ -0,0 +1,7 @@
+pub fn add_one(number:i32){
+    println!("The add one {}", number + 1)
+}
+
+pub fn add_two(number:i32) -> i32{
+    return  number + 2;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod functions;
 fn main() {
-
+    functions::add_one(1);
+    println!("The two add one {}", functions::add_two(1));
 }
